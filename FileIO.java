a70442ff79c5e349c670ddad873d843b4963c562
ssishi021 <ssishi020@student.wethinkcode.co.za>
2022-12-12 19:53:30 +0200

    Still working on the script

---
+++ FileIO.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileIO {
+  public static void main(String[] args) {
+
+    String[] names = {"Pieka Grobbelaar is a software Engineer and CEO of DrakkenTech",
+     "Siyabonga  Sishi is a final year student in software enginering looking for an internship", 
+     "WeThinkCode is in partnership with DrakkenTech helping student reach their goals in software engineering journey"};
+    
+
+
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+      writer.write("Writing to a file.");
+      writer.write("\nSummary between Pieka,Siya,WethinkCode and DrakkenTech:");
+      
+      for (String name : names) {
+        writer.write("\n" + name);
+      }
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+      String line;
+      while((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+      reader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
