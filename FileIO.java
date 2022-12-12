803fd22edcf7e4eb8b1ac17aa98eac34a2669e3b
ssishi021 <ssishi020@student.wethinkcode.co.za>
2022-12-12 20:19:33 +0200

    Changed the textfile

--- FileIO.java
+++ FileIO.java
@@ -8,7 +8,7 @@ public class FileIO {
   public static void main(String[] args) {
 
     String[] names = {"Pieka Grobbelaar is a software Engineer and CEO of DrakkenTech",
-     "Siyabonga Sishi is a final year student in software enginering looking for an internship", 
+     "Siyabonga Sishi is a final-year software engineering student looking for an internship with the possibility of full-time employment with Drakkentech.", 
      "WeThinkCode is in partnership with DrakkenTech helping student reach their goals in software engineering journey"};
     
     try {
